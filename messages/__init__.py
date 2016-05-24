3f8e4fb4e9dda957df02b29fd0bad9c071368eee
qstokkink <goqs@hotmail.com>
2016-05-24 16:02:56 +0200

    Added Tribler message definitions

--- messages/__init__.py
+++ messages/__init__.py
@@ -1,4 +1,11 @@
 def __import_handle(name):
     return __import__(name, globals(), locals(), [], -1)
 
-MESSAGES = []
+MESSAGES = [__import_handle('allchannel_pb2'),
+            __import_handle('bartercast4_pb2'),
+            __import_handle('channel_pb2'),
+            __import_handle('demers_pb2'),
+            __import_handle('multichain_pb2'),
+            __import_handle('search_pb2'),
+            __import_handle('template_pb2'),
+            __import_handle('tunnel_pb2')]
